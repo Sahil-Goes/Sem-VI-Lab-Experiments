1ff0e7e5434612fadc8a0e06ebe411870e9244d9
Sahil Goes <sahilgoes87@gmail.com>
2025-12-17 14:56:50 +0530

    Reorganize facts and rules in p3.pl

--- AI_Lab/Exp8/p3.pl
+++ AI_Lab/Exp8/p3.pl
@@ -1,9 +1,10 @@
 % Facts
 likes(john, mary).
 likes(john, susie).
-likes(john, susie) :- likes(john, mary).
 likes(X, susie).      % Everyone likes Susie
 likes(john, X).       % John likes everybody
 likes(X, john).       % Everybody likes John
 dislikes(john, pizza).
 
+% Rules
+likes(john, susie) :- likes(john, mary).
