032a32a3ff16923065a00ef21c7a3e9bf5f27ca9
Sahil Goes <sahilgoes87@gmail.com>
2025-12-31 22:51:42 +0530

    Added prolog 10

---
+++ AI_Lab/Exp8/p10.pl
@@ -0,0 +1,7 @@
+category(chair,furniture).
+category(table,furniture).
+category(bed,furniture).
+category(spoon,utensil).
+category(fork,utensil).
+category(knife,utensil).
+category(laptop,electronic_device).
